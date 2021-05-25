806d8ef108a65ff68601c9d19181dcbaa811af81
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2021-05-25 13:10:42 +0900

    Create def_createdbymyself.py

---
+++ def_createdbymyself.py
@@ -0,0 +1,55 @@
+def outlier_IQR(df, col, w=1.5):
+    q25 = np.percentile(df[col].values, 25)
+    q75 = np.percentile(df[col].values, 75)
+
+    IQR = q75 - q25
+    l_est = q25 - (IQR*w)
+    h_est = q75 + (IQR*w)
+
+    o_idx = df[col][(df[col]<l_est)|(df[col]>h_est)].index
+
+    return o_idx
+
+def zscr_idx(data, col):
+    mean = data[col].mean()
+    std = data[col].std()
+    
+    idx = 0
+    otrl = []
+    otrl_idx = []
+    
+    for val in data[col]:
+        if ((val-mean)/std>3)|((val-mean)/std<-3):
+            otrl.append(val)
+            otrl_idx.append(idx)
+        idx+=1
+
+    #otrl = [val for val in data[col] if ((val-mean)/std>3)|((val-mean)/std<-3)]
+    
+    return otrl, otrl_idx
+    #zscr_idx[0]=>otrl, zscr_idx[1]=>otrl_idx
+    
+def del_outlier(df,o_idx):
+    df = df.drop(o_idx, axis=0, inplace=True)
+  
+    return df
+
+def binning_IQR(df, col):
+    q25 = np.percentile(df[col].values, 25)
+    q50 = np.percentile(df[col].values, 50)
+    q75 = np.percentile(df[col].values, 75)
+    
+    belowq25 = df[col][df[col]<q25].values
+    q25toq50 = df[col][(df[col]>=q25)&(df[col]<q50)].values
+    q50toq75 = df[col][(df[col]<q75)&( df[col]>=q50)].values
+    aboveq75 = df[col][df[col]>=q75].values
+    
+    df[col] = df[col].replace(belowq25,0)
+    df[col] = df[col].replace(q25toq50,1)
+    df[col] = df[col].replace(q50toq75,2)
+    df[col] = df[col].replace(aboveq75,3)
+
+    df[col] = df[col].astype('int64')
+    
+    return df[col]
+
