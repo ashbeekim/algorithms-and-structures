d32dff516db4714658218442c540a603189426e6
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2021-05-27 18:25:29 +0900

    Update load_packages.py

--- load_packages.py
+++ load_packages.py
@@ -1,13 +1,62 @@
+##################################### Connect between colab and kaggle #####################################
+#구글드라이브 마운트
+import os
+from google.colab import drive  
+drive.mount('/content/drive')
+
+# 파일 업로드
+from google.colab import files
+files.upload()
+
+#코랩-캐글
+# Ensure kaggle.json is in the location ~/.kaggle/kaggle.json to use the API.
+!mkdir -p ~/.kaggle #sample_data에 저장됨
+!cp kaggle.json ~/.kaggle
+# Permission Warning 이 일어나지 않도록 
+!chmod 600 ~/.kaggle/kaggle.json
+!cd ~/.kaggle  
+############################################################################################################
+
+##################################### Mount colab to google drive #####################################
+#구글 드라이브에 캐글 데이터 다운로드
+import os  
+os.environ['KAGGLE_CONFIG_DIR'] = "~/.kaggle/"
+from google.colab import drive  
+drive.mount('/content/drive')
+!cd ~/.kaggle  
+
+# # # 본인이 참가한 모든 대회 보기 
+!kaggle competitions list
+# 캐글 데이터셋 리스트 확인
+!kaggle datasets list
+#캐글 데이터셋 리스트 검색
+!kaggle datasets list -s Movies
+# !kaggle competitions download -c house-prices-advanced-regression-techniques
+# !kaggle datasets download -d percevalw/englishfrench-translations  
+# !unzip englishfrench-translations.zip  
+# !ls
+#######################################################################################################
+
 ##################################### Basic #####################################
+import os
 import sys
 import numpy as np
 import pandas as pd
+from tqdm.notebook import trange, tqdm
+import json
+
+# 최대 줄 수 설정
+pd.set_option('display.max_rows', 1500)
+# 최대 열 수 설정
+pd.set_option('display.max_columns', 1500)
+# 표시할 가로의 길이
+pd.set_option('display.width', 1500)
 ###################################################################################
 
 ##################################### Math #####################################
 import math
 from math import sin, cos, sqrt, atan2, radians
-###################################################################################
+################################################################################
 
 ##################################### Time #####################################
 import time
@@ -17,7 +66,7 @@ from datetime import datetime
 import datetime as dt
 import calendar
 import holidays
-###################################################################################
+################################################################################
 
 ##################################### Visualization #####################################
 import matplotlib.pyplot as plt
@@ -35,22 +84,31 @@ import pygal
 import gleam
 import bokeh
 import missingo
-###################################################################################
+########################################################################################
 
 ##################################### Geometry #####################################
+import haversine as hs
+from haversine import haversine, Unit
+
 from folium import FeatureGroup, LayerControl, Map, Marker
 from folium.plugins import HeatMap
 import folium
 
 import geoplotlib
-###################################################################################
+
+import mapboxgl
+mapboxgl.__version__
+MAPBOX_API_KEY = "자신의 mapbox api token (pk... 로 시작함)"
+from mapboxgl.viz import *
+import pydeck as pdk
+####################################################################################
 
 ##################################### Seperate & Preprocessing & Engineering #####################################
 from sklearn.model_selection import train_test_split, cross_val_score, KFold, StratifiedKFold, GroupKFold
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.impute import SimpleImputer
-###################################################################################
+##################################################################################################################
 
 ##################################### Machine Learning #####################################
 #from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier)
@@ -77,19 +135,36 @@ from sklearn.impute import SimpleImputer
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
-###################################################################################
+########################################################################################
 
-##################################### NY_TAXI #####################################
+##################################### Evaluation #####################################
+from sklearn.metrics import roc_curve
+from sklearn.metrics import recall_score, f1_score, roc_auc_score 
+from sklearn.metrics import accuracy_score, precision_score, precision_recall_curve
+from sklearn.metrics import explained_variance_score
+######################################################################################
 
+##################################### Analysis #####################################
+import statsmodels.api as sm
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import classification_report, confusion_matrix
+from xgboost import plot_importance
+####################################################################################
 
-###################################################################################
+##################################### Setting #####################################
+import warnings
+warnings.simplefilter('ignore')
+warnings.filterwarnings('ignore')
+import gc
+gc.collect()
+import subprocess
 
-##################################### NY_TAXI #####################################
+# # # tips(from 현호님)  #속도 빨라짐
+# from numba import cuda, jit, prange, vectorize, guvectorize
+# #numba
+# @jit(nopython=True)
+# #prange 병렬화
+# @jit(nopython=True, parallel=True)
+####################################################################################
 
-
-###################################################################################
-
-##################################### NY_TAXI #####################################
-
-
-###################################################################################
+#[package list] https://pandas.pydata.org/pandas-docs/stable/ecosystem.html?highlight=geo
