cd1aca824bf4788a05afd86cba3cd9110bc06569
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-02-28 00:50:19 +0900

    Update step_2.py
    BAEKJOON(if)

--- step_2.py
+++ step_2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # if
-# # 1
+# 1330
 A, B = map(lambda x: int(x), input().split())
 if A>B:
     print('>')
@@ -9,7 +9,7 @@ elif A<B:
 else:
     print('==')
     
-# # 2
+# 9498
 score = int(input())
 if score in range(90,101):
     print('A')
@@ -22,7 +22,7 @@ elif score in range(60,70):
 else:
     print('F')
     
-# # 3
+# 2753
 year = int(input())
 if year%400==0:
   print(1)
@@ -31,7 +31,7 @@ elif year%4==0 and year%100!=0:
 else: 
   print(0)
   
-# # 4
+# 14681
 X = int(input())
 Y = int(input())
 if X > 0:
@@ -39,14 +39,14 @@ if X > 0:
 else:
     print(2) if Y > 0 else print(3)
     
-# # 5
+# 2884
 H, M = map(int, input().split())
 time = H*60 + M
 time = (time - 45) if (time - 45 >= 0) else (time - 45 + 24*60)
 H, M = time//60, time%60
 print(" ".join([str(H), str(M)]))
 
-# # 6
+# 2525
 A, B = map(int, input().split(' '))
 C = int(input())
 assert A in range(24)
@@ -62,3 +62,18 @@ else:
 if H > 23:
     H = H - 24
 print(H, M, sep=' ')
+
+# 2480
+dice = dict([(i+1,0) for i in range(6)])
+nums = list(map(lambda x: int(x), input().split()))
+for i in nums:
+    assert (i>=1)&(i<=6)
+    dice[i] += 1
+price = 0
+sortDice = sorted(dice.items(), key=lambda x: (-x[1], -x[0]))
+if sortDice[0][1] == 3:
+    print(10000 + sortDice[0][0] * 1000)
+elif sortDice[0][1] == 2:
+    print(1000 + sortDice[0][0] * 100)
+else:
+    print(sortDice[0][0] * 100)
