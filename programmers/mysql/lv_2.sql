4c837b0dc343437eed14f8d1fe828049cc859c6c
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-03-01 00:38:05 +0900

    Update programmers/mysql
    Update lv_2.sql
    Update lv_4.sql

--- programmers/mysql/lv_2.sql
+++ programmers/mysql/lv_2.sql
@@ -5,6 +5,10 @@ GROUP BY animal_type
 ORDER BY animal_type;
 
 -- 루시와 엘라 찾기
+SELECT animal_id, name, sex_upon_intake
+FROM animal_ins
+WHERE name IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+ORDER BY animal_id;
 
 -- 최솟값 구하기
 SELECT MIN(datetime) FROM animal_ins;
@@ -16,6 +20,10 @@ HAVING count >= 2
 ORDER BY name;
 
 -- 이름에 el이 들어가는 동물 찾기
+SELECT animal_id, name
+FROM animal_ins
+WHERE animal_type = "Dog" AND name LIKE '%el%'
+ORDER BY name;
 
 -- 동물 수 구하기
 SELECT COUNT(1) FROM animal_ins;
@@ -30,12 +38,23 @@ ORDER BY HOUR(datetime);
 SELECT animal_type, IFNULL(name, "No name") AS name, sex_upon_intake FROM animal_ins;
 
 -- 중성화 여부 파악하기
+SELECT animal_id, name, 
+    IF(LEFT(sex_upon_intake, 6) IN ("Neuter", "Spayed"), "O", "X") as "중성화"
+FROM animal_ins
+ORDER BY animal_id;
 
 -- 중복 제거하기
 SELECT COUNT(DISTINCT name) FROM animal_ins;
 
 -- DATETIME에서 DATE로 형 변환
+SELECT animal_id, name,
+    DATE_FORMAT(datetime, "%Y-%m-%d") AS "날짜"
+FROM animal_ins
+ORDER BY animal_id;
 
 /*
 MAX, MIN이 level1, level 2로 나뉘는 건 아직도 이유를 모르겠음.
+"루시와 엘라 찾기"를 CONCAT으로 작성한 분이 계셔서, STRING_SPLIT으로 풀어보려는 시도를 했으나 실패함.
+
+DATE_FORMAT은 MySQL 공식 문서에서 참고하는 게 제일 빠름.
 */
--- programmers/mysql/lv_4.sql
+++ programmers/mysql/lv_4.sql
@@ -5,12 +5,24 @@ SET @HOUR_RANGE = -1;
 SELECT  (@HOUR_RANGE := @HOUR_RANGE + 1) AS 'Hour',
     (SELECT COUNT(*) FROM animal_outs WHERE HOUR(datetime) = @HOUR_RANGE) AS 'Count'
 FROM animal_outs
-WHERE @HOUR_RANGE < 23;
+WHERE @HOUR_RANGE < 23;       
 
 -- 보호소에서 중성화한 동물
+SELECT outs.animal_id, outs.animal_type, outs.name
+FROM animal_outs outs 
+LEFT JOIN animal_ins ins 
+    ON outs.animal_id = ins.animal_id
+    AND outs.sex_upon_outcome <> ins.sex_upon_intake
+WHERE ins.sex_upon_intake IS NOT NULL
+ORDER BY outs.animal_id;
+
 
 /*
 이쯤에서 뭔가 SQL 고득점 Kit와 모든 문제는 다른 기준을 가지고 있음을 확신함.
+
+"보호소에서 중성화한 동물" 구조 당시 중성화 전이었던 아이들 중 입양된 아이들을 중성화했다면, 입양된 테이블에서 조회하는 게 더 빠른 결과를 도출할 것이란 생각에 위와 같이 작성함.
+처음엔 NULL값을 고려하지 못 하고 제출했는데, 틀렸대서 select에 값을 추가해서 확인해보니 데이터 누락에 대한 처리를 해야함을 알 수 있었음.
+
 남은 문제는 이번 주 내로 업데이트 할 예정임.
 현재 기준으로 level 5는 MySQL, Oracle 모두 없음.
 */
