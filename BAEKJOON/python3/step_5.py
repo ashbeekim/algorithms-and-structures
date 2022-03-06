515033a9107b3c91cbc870077272a2407496d5f2
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-03-07 01:10:44 +0900

    Update baekjoon/python3
    Update step_5.py

--- BAEKJOON/python3/step_5.py
+++ BAEKJOON/python3/step_5.py
@@ -17,11 +17,42 @@ print(max(nums.keys()))
 print(nums.get(max(nums.keys())))
 
 # 2577
+a = int(input())
+b = int(input())
+c = int(input())
+n_bucket = {i:0 for i in range(10)}
+num = list(map(int, list(str(a*b*c))))
+for i in num:
+    n_bucket[i] += 1
+for j, k in n_bucket.items():
+    print(k)
 
 # 3052
+cnt, arr = 0, []
+while cnt < 10:
+    num = int(sys.stdin.readline())
+    arr.append(num%42)
+    cnt += 1
+print(len(set(arr)))
 
 # 1546
+n = int(input())
+arr = list(map(int, input().split()))
+new = list(map(lambda x: x/max(arr)*100, arr))
+print(sum(new)/n)
 
 # 8958
+n = int(input())
+for i in range(n):
+    l = sys.stdin.readline()
+    pts, score = 0, 0
+    for j in l:
+        if j=='O':
+            pts += 1
+            score += pts
+        else:
+            pts = 0
+    print(score)
 
 # 4344
+
