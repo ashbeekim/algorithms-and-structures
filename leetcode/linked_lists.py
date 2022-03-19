75e60e9dc6fb211b2bb1e3bf580d9183fe91ed09
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-03-19 22:59:05 +0900

    Update leetcode
    Create linked_lists.py
    isPalindrome을 런너를 이용해서 풀이하는 건 아직 이해가 되지 않아서, deque로만 풀이함

---
+++ leetcode/linked_lists.py
@@ -0,0 +1,27 @@
+import re
+from typing import Optional, Deque
+from collections import deque
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q: Deque = deque()
+
+        if not head:
+            return True
+
+        node = head
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+        
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+        
+        return True
