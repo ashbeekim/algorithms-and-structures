20477f098208e79e6c8a8aa4f6f096e41b0f23f5
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-04-18 16:29:59 +0900

    Update programmers/python3
    * sort.py
        * H-index : map 활용도를 높여야 함. 그리고 해당 문제는 문제에 대한 설명(?), 의역이 부족함.
    * brute-force.py
        * 카펫 : 특히 브루트 포스의 경우, 반복의 범주 및 loop 탈출 조건을 잘 설정하기가 중요함.

--- programmers/python3/brute-force.py
+++ programmers/python3/brute-force.py
@@ -1,3 +1,7 @@
+from itertools import permutations
+from math import sqrt, floor
+
+
 # 모의고사
 def solution(answers):
     p_1 = [1, 2, 3, 4, 5]
@@ -18,9 +22,6 @@ def solution(answers):
     return answer
 
 # 소수찾기
-from itertools import permutations
-from math import sqrt, floor
-
 def solution(numbers):
     numbers = list(sorted(numbers))
     nums, num = [], []
@@ -36,7 +37,20 @@ def solution(numbers):
     return len(num)
     
 # 카펫
+def solution(brown, yellow):
+    total = brown + yellow
+    for row in range(3, total//2):
+        col = total // row
+
+        if (((row * col) == total) & (row >= col) & (((col-2) * (row-2)) == yellow)):                
+            answer = [row, col]
+            break
+            
+    return answer
+
 
 '''
-
+카펫 문제를 풀이할 때 고려한 내용
+전체 카펫의 가로 혹은 세로는 무조건 3 이상일 수 밖에 없음(brown|yellow|brown 순의 샌드 형식이기 때문)
+가로는 세로와 같거나 세로보다 길다고 했기에 전체 타일의 수에서 절반 이하의 범주에 속할 수 있음
 '''
--- programmers/python3/sort.py
+++ programmers/python3/sort.py
@@ -19,8 +19,19 @@ def solution(numbers):
 
 # H-Index
 def solution(citations):
-    answer = 0
-    return answer
+    citations.sort(reverse = True)
+    
+    for num in range(len(citations), 0, -1):
+        count = 0
+        for cnt in  citations:
+            if cnt >= num:
+                count += 1
+        
+        if count >= num:
+            return num
+        elif count == 0:
+            return 0
+
     
 '''
 k번째 수를 제출한 뒤, 람다식으로 풀이한 내용을 봤을 땐..ㅋㅋㅋㅋㅋ 함수로 작성하라에 사로잡히지 말자는 다짐을 하게 됨.
@@ -28,4 +39,10 @@ def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 가장 큰 수를 제출할 때, 2를 초과하는 어떤 수를 넣어도 출력은 되지만 numbers의 원소는 1000 이하라는 점에서 "9"*3="999"가 되니까 큰 수부터 정렬하면 결국 원하는 결과값이 나옴.
 제한을 준 내용에서 그 이상의 시도를 할 필요가 없음을 알았지만, 확연하게 연산 속도에 차이가 나기 시작한 시점이 10 정도였던 것으로 기억함.
+
+H-index를 심플하게 풀이한 코드가 있어서 남겨봄.
+def solution(citations):
+    citations.sort(reverse=True)
+    return max(map(min, enumerate(citations, start=1)))
+이렇게 풀이한 것을 보고, 여태껏 map을 제대로 활용하지 못했다는 것을 직시할 수 있었음.
 '''
