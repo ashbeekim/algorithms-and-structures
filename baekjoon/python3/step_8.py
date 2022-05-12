5c01bbcb9fa8ac9b35633fe210de9835cedcf126
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-05-12 23:49:31 +0900

    Update baekjoon/python3/step_8.py
        Solve 1978, 2581 두 문제에서 사용된 함수는 소수를 bool 형태로 반환하는 함수로 작성
    다만 요구하는 출력 형태가 달랐기에, 추가적인 내용은 filter와 if-else로 처리함.

--- baekjoon/python3/step_8.py
+++ baekjoon/python3/step_8.py
@@ -3,8 +3,31 @@ import math
 
 
 # 1978
+def isprime(num):
+    if num==1:
+        return False
+    else:
+        for n in range(2, int(num**.5)+1):
+            if num%n==0:
+                return False
+        return True
+N = int(input())
+arr = list(map(int, input().split()))
+arr = list(map(isprime, arr))
+arr = list(filter(lambda x: x==True, arr))
+print(len(arr))
 
 # 2581
+a = int(input())
+b = int(input())
+arr = list(map(lambda x, y: (x, isprime(y)), range(a, b+1), range(a, b+1)))
+arr = list(filter(lambda x: x[1]==True, arr))
+if arr:
+    arr = dict(arr)
+    print(sum(arr.keys()))
+    print(min(arr.keys()))
+else:
+    print(-1)
 
 # 11653
 def factorization(x): 
@@ -44,3 +67,8 @@ while True:
 # 3053
 
 # 1002
+
+"""
+# 1978, 2581은 동일하게 소수를 찾는 함수를 사용하기에 코드 중복 방지 차원에서 상단에 작성함.
+    처음에 분명 소수를 구하는 값이라고 생각하고 작성했었는데, 1에 대한 예외처리를 하지 않아서 틀렸었음.
+"""
