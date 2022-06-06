4ab00d9e640450b3b3ce4d3eb953350f72183d04
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-06-06 20:37:30 +0900

    Update baekjoon/python3/step_7.py
            Solve 2775, 결국 출력하고자 하는 게 무엇인지 잊지 않아야 하는 문제

--- baekjoon/python3/step_7.py
+++ baekjoon/python3/step_7.py
@@ -59,6 +59,16 @@ for _ in range(int(input())):
 
 
 # 2775
+T = int(sys.stdin.readline())
+for _ in range(T):
+    k = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
+    people = [i for i in range(1, n + 1)]
+
+    for _ in range(k):
+        for j in range(1, n):
+            people[j] += people[j - 1]
+    print(people[-1])
 
 
 # 2839
