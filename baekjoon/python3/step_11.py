ce247037e8d6f7b3961d3505cf57574e9059ef41
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-07-05 00:31:51 +0900

    Update baekjoon/python3/step_11.py
    Solve 2108, 최빈값 중복 처리에서 cnt(==max count) 기준으로 filter해서 작성함.

--- baekjoon/python3/step_11.py
+++ baekjoon/python3/step_11.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # sort
 import sys
+from collections import Counter
 
 
 # 2750
@@ -13,6 +14,25 @@ import sys
 
 
 # 2108
+N = int(sys.stdin.readline())
+arr = [int(sys.stdin.readline()) for _ in range(N)]
+arr.sort()
+if len(arr)==1:
+    print(arr[0])
+    print(arr[0])
+    print(arr[0])
+    print(0)
+else:
+    print(int(round(sum(arr)/N)))
+    print(arr[int(N/2)])
+    c_arr = sorted(Counter(arr).items(), key=lambda x: (-x[1], x[0]))
+    cnt = c_arr[0][1]
+    c_arr = list(filter(lambda x: x[1]==cnt, c_arr))
+    if len(c_arr)==1:
+        print(c_arr[0][0])
+    else:
+        print(c_arr[1][0])
+    print(arr[-1] - arr[0])
 
 
 # 1427
@@ -35,5 +55,5 @@ print('\n'.join(arr))
 
 
 """
-
+# 2108, 최빈값 중복 시 처리 규칙에서 약간 고민했던 문제.
 """
