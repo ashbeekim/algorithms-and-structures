c8f9065f9a9126b0031f88967d45247890d1016a
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-08-09 20:51:35 +0900

    Update leetcode/extra_easy.py
        Solve 26. Remove Duplicates from Sorted Array

--- leetcode/extra_easy.py
+++ leetcode/extra_easy.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def isPalindrome(self, x: int) -> bool: # 9. Palindrome Number
         if x == 0:
@@ -40,3 +43,13 @@ class Solution:
         for _ in range(cnt):
             nums.remove(0)
             nums.append(0)
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+        return j + 1
