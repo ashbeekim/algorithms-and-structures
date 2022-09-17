bb9dc2684b19158c1b933a38ce83e2c081e95a77
Ashbee Kim <81610017+AshbeeKim@users.noreply.github.com>
2022-09-17 14:47:46 +0900

    Update leetcode/extra_medium.py
    Solve 48. Rotate Image
        * 33ms, faster than 97.13%
        * 13.8MB, less than 73.93%

--- leetcode/extra_medium.py
+++ leetcode/extra_medium.py
@@ -34,8 +34,24 @@ class Solution:
                 cnt += 1
         return sum(diff)
 
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
             nums[i] = max(nums[i], nums[i] + nums[i - 1])
         return max(nums)
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        leng = len(matrix) # n by n
+        depth = leng // 2
+        for i in range(depth):
+            turn, dst = leng - 2 * i - 1, leng - 1 - i
+            for j in range(turn):
+                # rotate
+                temp = matrix[i][i+j]
+                matrix[i][i+j] = matrix[dst-j][i]
+                matrix[dst-j][i] = matrix[dst][dst-j]
+                matrix[dst][dst-j] = matrix[i+j][dst]
+                matrix[i+j][dst] = temp
