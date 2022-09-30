d0b5c09b2e84fdb3f7b8eeb9462fb2710a028197
Ashbee Kim <co_m@gnu.ac.kr>
2022-09-30 23:04:05 +0900

    Update baekjoon/step_12.py

--- baekjoon/python3/step_12.py
+++ baekjoon/python3/step_12.py
@@ -4,6 +4,22 @@ import sys
 
 
 # 10815
+n = int(input())
+n_lst = list(map(int, sys.stdin.readline().split()))
+m = int(input())
+m_lst = list(map(int, sys.stdin.readline().split()))
+
+_dict = {}
+for _ in range(n):
+    _dict[n_lst[_]] = 0
+
+res = ''
+for _ in range(m):
+    if m_lst[_] in _dict:
+        res += '1 '
+    else:
+        res += '0 '
+print(res)
 
 
 # 14425
@@ -30,6 +46,7 @@ print((len(comp) - len(A)) + (len(comp) - len(B)))
 
 
 """
+# 10815, dict 형식으로 선언하지 않으면, 시간 초과 발생
 # 1269, 람다식으로 선언해서 하니까 오히려 이번 문제에선 시간 초과가 문제가 되었음.
     차집합 구하는 방식으로 문제를 해결함.
 """
